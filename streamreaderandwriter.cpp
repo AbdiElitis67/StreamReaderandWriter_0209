0745dd6df78147b16c4c7f151d9f0298e4f32e6e
Abdielitis67 <meirdanz4@gmail.com>
2026-06-01 18:27:47 +0700

    nambahkan fitur input dan penyimpanan data ke file

--- streamreaderandwriter.cpp
+++ streamreaderandwriter.cpp
@@ -12,3 +12,19 @@ int main()
     // membuka file dalam mode menulis
     ofstream outfile;
 
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << "=> Menulis file, 'q' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis file
+    while (true)
+    {
+        cout << "- ";
+
+        // mendapat setiap karakter dalam satu baris
+        getline(cin, baris);
+
+        // kondisi false jika baris hanya berisi karakter q
+        if (baris == "q")
+            break;
